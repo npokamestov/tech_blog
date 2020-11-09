ceb065693d619aba530eb5203c0735fa9cdb6168
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-11-09 14:03:59 -0500

    undo secret change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const sequelize = require("./config/connection");
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-    secret: process.env.DB_SESSION_SECRET,
+    secret: 'Super secret secret',
     cookie: { maxAge: 8000000 },
     resave: false,
     saveUninitialized: true,
